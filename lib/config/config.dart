73fc5d14adde554d583e56a1abe715a17cd3efb7
Denis Bakhtin <denis.bakhtin@gmail.com>
2021-12-09 13:37:40 +0300

    fixed config

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -1,10 +1,43 @@
 import 'dart:convert';
 import 'dart:io';
 
+import 'package:path/path.dart';
 import 'package:itblog/models/helpers.dart';
 
 //application mode
 enum Mode { debug, test, release }
+
+class Configs {
+  final Config debug;
+  final Config test;
+  final Config release;
+
+  Configs(this.debug, this.test, this.release);
+  factory Configs.load() {
+    final _path = join(Directory.current.path, 'lib', 'config.json');
+    final _contents = File(_path).readAsStringSync();
+    return Configs._fromJson(json.decode(_contents));
+  }
+  factory Configs._fromJson(Map<String, dynamic> map) {
+    return Configs(
+      Config.fromJson(map['debug']),
+      Config.fromJson(map['test']),
+      Config.fromJson(map['release']),
+    );
+  }
+  Config mode(String m) {
+    switch (m) {
+      case 'debug':
+        return debug;
+      case 'release':
+        return release;
+      case 'test':
+        return test;
+      default:
+        throw 'Unsupported application mode';
+    }
+  }
+}
 
 class Config {
   final int port;
@@ -12,13 +45,7 @@ class Config {
   final String cookieSecret;
 
   Config(this.port, this.database, this.cookieSecret);
-  factory Config.load(Mode mode) {
-    final _modeStr = mode.toString().split('.').last;
-    final _path = 'lib/config/config_$_modeStr.json';
-    var _contents = File(_path).readAsStringSync();
-    return Config._fromJson(json.decode(_contents));
-  }
-  factory Config._fromJson(Map<String, dynamic> map) {
+  factory Config.fromJson(Map<String, dynamic> map) {
     return Config(
       toInt(map['port']),
       toStr(map['database']),
--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,14 @@ import 'package:shelf/shelf_io.dart' as io;
 import 'config/config.dart';
 import 'models/data.dart';
 import 'controllers/blog_router.dart';
+import 'package:args/args.dart';
 
-void main() async {
-  // You can setup context, database connections, cache connections, email
-  // services, before you create an instance of your service.
-  var config = Config.load(Mode.debug);
+void main(List<String> args) async {
+  final parser = ArgParser();
+  parser.addOption('mode', allowed: ['debug', 'release'], defaultsTo: 'debug');
+  final arguments = parser.parse(args);
+
+  final config = Configs.load().mode(arguments['mode']);
   var db = DB(config.database);
   final injector = Injector.appInstance;
   injector.registerSingleton<Config>(() => config);
@@ -19,5 +22,6 @@ void main() async {
   var service = BlogRouter();
   // Service request using the router, note the router can also be mounted.
   var server = await io.serve(service.handler, 'localhost', config.port);
+  print(config.port);
   print('Server listening on http://${server.address.host}:${server.port}');
 }
