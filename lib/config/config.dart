3ffb51f92f3f86187f0341491c9ef7aec5219d04
Denis Bakhtin <denis.bakhtin@gmail.com>
2021-12-23 16:14:31 +0300

    First commit

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -1,41 +1,8 @@
 import 'dart:convert';
 import 'dart:io';
 
-import 'package:path/path.dart';
 import 'package:itblog/models/helpers.dart';
 import 'package:path/path.dart';
-
-class Configs {
-  final Config debug;
-  final Config test;
-  final Config release;
-
-  Configs(this.debug, this.test, this.release);
-  factory Configs.load() {
-    final _path = join(Directory.current.path, 'lib', 'config.json');
-    final _contents = File(_path).readAsStringSync();
-    return Configs._fromJson(json.decode(_contents));
-  }
-  factory Configs._fromJson(Map<String, dynamic> map) {
-    return Configs(
-      Config.fromJson(map['debug']),
-      Config.fromJson(map['test']),
-      Config.fromJson(map['release']),
-    );
-  }
-  Config mode(String m) {
-    switch (m) {
-      case 'debug':
-        return debug;
-      case 'release':
-        return release;
-      case 'test':
-        return test;
-      default:
-        throw 'Unsupported application mode';
-    }
-  }
-}
 
 class Configs {
   final Config debug;
@@ -77,13 +44,9 @@ class Config {
   final String siteName;
   final String mode;
 
-<<<<<<< HEAD
-  Config(this.port, this.database, this.cookieSecret);
-=======
   Config(this.port, this.database, this.cookieSecret, this.isSignupEnabled,
       this.siteName, this.mode);
 
->>>>>>> sass
   factory Config.fromJson(Map<String, dynamic> map) {
     return Config(
       toInt(map['port']),
--- lib/main.dart
+++ lib/main.dart
@@ -7,38 +7,19 @@ import 'package:shelf_hotreload/shelf_hotreload.dart';
 
 import 'config/config.dart';
 import 'controllers/blog_router.dart';
-<<<<<<< HEAD
-import 'package:args/args.dart';
-=======
 import 'models/data.dart';
->>>>>>> sass
 
 void main(List<String> args) async {
   final parser = ArgParser();
   parser.addOption('mode', allowed: ['debug', 'release'], defaultsTo: 'debug');
   final arguments = parser.parse(args);
-<<<<<<< HEAD
-
-  final config = Configs.load().mode(arguments['mode']);
-=======
   final mode = arguments['mode'];
 
   final config = Configs.load().mode(mode);
->>>>>>> sass
   var db = DB(config.database);
   final injector = Injector.appInstance;
   injector.registerSingleton<Config>(() => config);
   injector.registerSingleton<DB>(() => db);
-<<<<<<< HEAD
-
-  // Create an instance of your service, usine one of the constructors you've
-  // defined.
-  var service = BlogRouter();
-  // Service request using the router, note the router can also be mounted.
-  var server = await io.serve(service.handler, 'localhost', config.port);
-  print(config.port);
-  print('Server listening on http://${server.address.host}:${server.port}');
-=======
   if (mode == 'debug') {
     withHotreload(() => createServer(config, mode));
   } else {
@@ -51,5 +32,4 @@ Future<HttpServer> createServer(Config config, String mode) {
       'Starting server with hotreload on http://localhost:${config.port} in $mode mode');
   var service = BlogRouter();
   return io.serve(service.rootHandler, 'localhost', config.port);
->>>>>>> sass
 }
